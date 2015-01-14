a83eb55960f72cb8952a4b4a973817e8d377e630
Kenshin <xiyingit@gmail.com>
2015-01-14 10:39:05 +0800

    ...

--- random/random.go
+++ random/random.go
@@ -20,6 +20,7 @@ func (this *RandByTime) RandIntArray(size, maxnum int) []int {
 	for i := 0; i < size; i++ {
 		arr[i] = this.r.Intn(maxnum)
 	}
+        return arr
 }
 
 func (this *RandByTime) RandIntString(size, maxnum int) string {
